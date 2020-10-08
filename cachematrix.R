3d4e8bb867dfd18a0a2d7210019ef86321bfd1ae
Efstratios D. Tserkezos <s.tserkezos@gmail.com>
2020-10-08 13:59:51 +0300

    Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To be honest it's just the same function as the assigment, slightly changed to be for a matrix and insted
+## of the mean to provide the inverse of a matrix
 
-## Write a short comment describing this function
+## You input a matrix and define an m that is Null (so there is nothing there) and a set function of y that pretty much defines y
+##as the matrix in question and m it's NULL value
+##it just makes a list to be used as input for the cacheSolve
+#####
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverto) m <<- inverto
+  getinv <- function() m
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## it takes the input from the above function, looks if m is null, if it is it displays a message, takes the data, 
+##solves it (gets the inverse through solve function) and it displays it
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
